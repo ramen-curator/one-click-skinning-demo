02b5848de2f74d1a484adf43cfa5ec430491dad9
赖铭志 <ramen.curator@gmail.com>
2022-12-08 23:09:19 +0800

    声明webpack插件，里面只有一个主题名字数组

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
+const fs = require("fs");
+const webpack = require("webpack");
+
+const themeNames = fs
+  .readdirSync("./src/style/theme")
+  .filter((item) => fs.lstatSync("./src/style/theme/" + item).isDirectory());
+
 module.exports = defineConfig({
   transpileDependencies: true,
   css: {
@@ -7,5 +14,15 @@ module.exports = defineConfig({
         additionalData: `@import "@/styles/theme/index.scss";`,
       },
     },
+  },
+  configureWebpack: (config) => {
+    return {
+      plugins: [
+        // 自定义webpack插件
+        new webpack.DefinePlugin({
+          THEME_NAMES: JSON.stringify(themeNames),
+        }),
+      ],
+    };
   },
 });
